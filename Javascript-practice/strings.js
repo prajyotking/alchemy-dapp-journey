9011c6bde779eba781e877d6cdcc767f6df25668
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-07-24 16:53:03 +0530

    first log in  strings.js

--- Javascript-practice/strings.js
+++ Javascript-practice/strings.js
@@ -1,1 +1,11 @@
-
+/* startsWithX function to determine if the first character of the string argument is the lower-case x.
+If the first character is x return true. If not, return false*/
+function startsWithX(string) {
+    if(string.toLower){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
