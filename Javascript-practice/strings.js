30539cf3dbef95515131e4e7ef52850e629e0bde
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-07-24 17:04:08 +0530

    fixed the logic in string.js

--- Javascript-practice/strings.js
+++ Javascript-practice/strings.js
@@ -1,7 +1,7 @@
 /* startsWithX function to determine if the first character of the string argument is the lower-case x.
 If the first character is x return true. If not, return false*/
 function startsWithX(string) {
-    if(string.toLower){
+    if(string[0]==="x"){
         return true;
     }
     else{
