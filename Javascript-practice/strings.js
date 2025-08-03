f6b1dd67fae80770455224d50afe0f8871144d66
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-08-03 18:16:19 +0530

    really tricky function.js

--- Javascript-practice/strings.js
+++ Javascript-practice/strings.js
@@ -62,3 +62,23 @@ module.exports = isAllX;
 
 // ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
+/* find the longer half of the string before and after the x!
+
+1. First, you'll need to find the lower-case x.
+2.Once you've found the x, split the string in half. The first half will be the string before the x, the second half will be the string after the x.
+3.Take the longer string and return it!*/
+
+function splitAtX(string) {
+    const index = string.indexOf("x");
+    const stra = string.slice(0,index);
+    const strb = string.slice(index+1);
+    return (stra.length > strb.length) ? stra:strb;
+
+}
+/*OutPut :-
+splitAtX("Happyxdays"); // "Happy"
+splitAtX("10xDeveloper"); // "Developer"*/
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
