8b07b5f8e474495f442e4d64acccee81c1d703fc
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-09-21 18:29:50 +0530

    Update array.js

--- Javascript-practice/array.js
+++ Javascript-practice/array.js
@@ -38,6 +38,21 @@ function unique(array) {
 }
 //---------------------------------------------------------------------------------------------------------------------------------------------------------
 
+/*Given an array of integers and a number, num, find all the occurrences of the number and remove it from the array. 
+ Modify the array directly and do not return anything from this function.*/
+
+function removeOccurrences(array, num) {
+    for(i = array.length-1; i>=0; i--){
+        if(array[i] === num){
+            array.splice(i, 1);
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
