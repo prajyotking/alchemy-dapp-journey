ed6d9208d445b06bf9e537e2a43558a8a6ffec79
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-09-22 22:54:47 +0530

    Update objects.js

--- Javascript-practice/objects.js
+++ Javascript-practice/objects.js
@@ -1,1 +1,28 @@
+// This is my journal about completing objects goals & challenges on alchemy
+
+//--------------------------------------------------------------------------------------
+
+/*Given an array of pizza orders, return the total number of pizzas ordered.
+  The orders are an array of objects, each with pizzas key inside:*/
+const orders = [
+    { pizzas: 3 },
+    { pizzas: 5 },
+    { pizzas: 10 }
+];
+
+
+function numberOfPizzas(orders) {
+    let count = 0;
+    for(let i = 0; i< orders.length; i++){
+       
+         count += orders[i].pizzas;
+     
+    }
+    return count;
+    
+}
+
+//--------------------------------------------------------------------------------------
+
+
 
