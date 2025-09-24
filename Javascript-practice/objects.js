fa446a380f55cf99c3763ae1d09894f2c6dccc07
Prajyot Ghosalkar <prajyotghosalkar2004@gmail.com>
2025-09-24 20:28:24 +0530

    Update objects.js

--- Javascript-practice/objects.js
+++ Javascript-practice/objects.js
@@ -1,6 +1,6 @@
 // This is my journal about completing objects goals & challenges on alchemy
 
-//--------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 /*Given an array of pizza orders, return the total number of pizzas ordered.
   The orders are an array of objects, each with pizzas key inside:*/
@@ -22,7 +22,46 @@ function numberOfPizzas(orders) {
     
 }
 
-//--------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Modify the numberOfPizzas function to only count pizzas when the order.type is equal to ORDER_TYPES.PIZZA.
+
+//ordersList-->
+
+const orders = [
+    { pizzas: 3, type: ORDER_TYPES.PIZZA },
+    { wings: 12, type: ORDER_TYPES.WINGS },
+];
+
+
+//orderType.js
+
+const ORDER_TYPES = {
+    PIZZA : 0,
+    WRAPS : 2,
+    CHIKEN_DRUMSTICK : 4,
+    BURGERS : 1
+    }
+
+//numberOfPizzas.js
+
+const ORDER_TYPES = require('./orderTypes');
+
+function numberOfPizzas(orders) {
+    let total = 0;
+    for(let i = 0; i < orders.length; i++) {
+        if (orders[i].type === ORDER_TYPES.PIZZA) {
+            total += orders[i].pizzas;
+        }
+    }
+    return total;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
